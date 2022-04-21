1c678546a2f2fbc92a4a459dc81c3cbf7c3b0c9f
Tamar Ervin <tamarervin@gmail.com>
2022-04-21 14:16:22 -0700

    modules

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='SolAster',
-    version='1.0.5',
+    version='1.0.6',
     packages=['SolAster', 'SolAster.tools', 'SolAster.source', 'SolAster.examples'],
     install_requires=['astropy', 'sunpy', 'scikit-image', 'SolAster.tools', 'SolAster.source', 'SolAster.examples'],
     url='https://github.com/tamarervin/SolAster',
